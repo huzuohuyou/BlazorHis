2cf29f4c0d4960630727d5be01d70e6d613cd4aa
DESKTOP-6NPNRJ4\吴海龙 <13126506430@163.com>
2021-02-05 17:25:56 +0800

    .

--- src/BlazorHis.Blazor/Program.cs
+++ src/BlazorHis.Blazor/Program.cs
@@ -14,9 +14,9 @@ namespace BlazorHis.Blazor
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
-
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:44309") });
+           
+            //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:44309") });
             builder.Services.AddAntDesign();
             builder.Services.Configure<ProSettings>(builder.Configuration.GetSection("ProSettings"));
 
